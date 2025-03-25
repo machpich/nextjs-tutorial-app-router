2ddff270ea704455bb66b02ecde895658cc956ce
obama shoko <shoko.obama@kufu.co.jp>
2025-03-26 08:17:56 +0900

    Chap.14

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -9,63 +9,90 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+			invalid_type_error: 'Please select a customer.',
+		}),
+    amount: z.coerce.number()
+		  .gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['pending', 'paid'], {
+			invalid_type_error: 'Please select an invoice status.',
+		}),
     date: z.string()
 })
 
+export type State = {
+	errors?: {
+		customerId?: string[];
+		amount?: string[];
+		status?: string[];
+	};
+	message?: string | null;
+};
+
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-    });
-    const amountInCents = amount * 100;
-    const date = new Date().toISOString().split('T')[0];
+export async function createInvoice(prevState: State, formData: FormData) {
+	const validatedFields = CreateInvoice.safeParse({
+		customerId: formData.get('customerId'),
+		amount: formData.get('amount'),
+		status: formData.get('status'),
+	});
+	
+	if (!validatedFields.success) {
+		return {
+			errors: validatedFields.error.flatten().fieldErrors,
+			message: 'Missing Fields. Failed to Create Invoice.',
+		};
+	}
 
-    try {
-        await sql`
-            INSERT INTO invoices (customer_id, amount, status, date)
-            VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-        `;
-    } catch (error) {
-        console.log('データベースへの挿入に失敗しました', error);
-    }
+	const { customerId, amount, status } = validatedFields.data;
+	const amountInCents = amount * 100;
+	const date = new Date().toISOString().split('T')[0];
 
-    revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
+	try {
+			await sql`
+					INSERT INTO invoices (customer_id, amount, status, date)
+					VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+			`;
+	} catch (error) {
+			console.log('データベースへの挿入に失敗しました', error);
+	}
+
+	revalidatePath('/dashboard/invoices');
+	redirect('/dashboard/invoices');
 }
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
-    // フォームデータを解析して、customerId, amount, statusを取得
-    const { customerId, amount, status } = UpdateInvoice.parse({
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-    });
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+	const validatedFields = UpdateInvoice.safeParse({
+		customerId: formData.get('customerId'),
+		amount: formData.get('amount'),
+		status: formData.get('status'),
+	});
 
-    // 金額をセントに変換
-    const amountInCents = amount * 100;
+	if (!validatedFields.success) {
+		return {
+			errors: validatedFields.error.flatten().fieldErrors,
+			message: 'Missing Fields. Failed to Update Invoice.',
+		};
+	}
 
-    // データベースの更新
-    try {
-        await sql`
-            UPDATE invoices
-            SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-            WHERE id = ${id}
-        `;
-    } catch (error) {
-        console.log('データベースの更新に失敗しました', error);
-    }
+	const { customerId, amount, status } = validatedFields.data;
+	const amountInCents = amount * 100;
 
-    // キャッシュの再検証
-    revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
+	try {
+			await sql`
+					UPDATE invoices
+					SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+					WHERE id = ${id}
+			`;
+	} catch (error) {
+			console.log('データベースの更新に失敗しました', error);
+	}
+
+	revalidatePath('/dashboard/invoices');
+	redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
